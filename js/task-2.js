d2ffb699214db2b8b87473049b2cf66258901eb9
Vita Giricheva <nikaangel295@gmail.com>
2024-05-13 16:59:14 +0100

    add task

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,9 @@
 function getShippingMessage(country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
 };
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
